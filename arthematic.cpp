eee0746886879497d4694e494375d271b7cf6a9e
sowjanya <sowji81089@gmail.com>
2021-12-26 13:49:20 +0530

    multiplication function has been added

---
+++ arthematic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void add(int x, int y){
+    int z = x + y;
+    std::cout << "z" << z << std::endl;
+}
+
+void mult(int p, int q){
+     int r = s * t;
+     std::cout << "r" << r << std::endl;
+}
+
+int main(){
+    int a = 10, b = 20;
+    add(a, b);
+    mult(a, b);
+}
+
